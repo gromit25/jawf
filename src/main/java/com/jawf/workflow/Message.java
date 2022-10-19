30bfb802bb7c55ce5d67df39ea9a1ce3b3787fdb
gromit25 <gromit25@hanmail.net>
2022-10-19 22:19:52 +0900

    10/19

--- src/main/java/com/jawf/workflow/Message.java
+++ src/main/java/com/jawf/workflow/Message.java
@@ -5,12 +5,64 @@ import java.util.HashMap;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * 
+ * @author jmsohn
+ */
 public class Message {
 	
 	@Getter
 	@Setter
 	private int priority;
 	
+	/** */
+	private HashMap<String, Class<?>> types;
+	/** */
 	private HashMap<String, Object> data;
+	
+	
+	public <T> T get(String colName, Class<T> type) {
+		return type.cast(this.getData().get(colName));
+	}
+	
+	/**
+	 * 
+	 * @param colName
+	 * @return
+	 */
+	public String getString(String colName) throws Exception {
+		
+		if(this.getData().containsKey(colName) == false) {
+			throw new Exception("Column is not found: " + colName);
+		}
+		
+		return this.getData().get(colName).toString();
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private HashMap<String, Class<?>> getTypes() {
+		
+		if(this.types == null) {
+			this.types = new HashMap<String, Class<?>>();
+		}
+		
+		return this.types;
+	}
+	
+	/**
+	 * 
+	 * @return
+	 */
+	private HashMap<String, Object> getData() {
+		
+		if(this.data == null) {
+			this.data = new HashMap<String, Object>();
+		}
+		
+		return this.data;
+	}
 
 }
