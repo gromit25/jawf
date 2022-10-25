694331c92bb35ea07236f22b5e656a41923a98d0
gromit25 <gromit25@hanmail.net>
2022-10-25 22:10:38 +0900

    AbstractParser 클래스에 주석 추가

--- src/main/java/com/jawf/parser/AbstractParser.java
+++ src/main/java/com/jawf/parser/AbstractParser.java
@@ -29,33 +29,45 @@ public abstract class AbstractParser<T> {
 	 */
 	public AbstractParser() throws Exception {
 		
-		// 
+		// 파싱 트리 노드 생성
 		this.node = new TreeNode<T>();
 		
 		// transfer event handler 목록 생성
 		this.transferHandlers = new HashMap<String, HashMap<String, ArrayList<Method>>>();
 		
 		// 하위 클래스에서 구현된 transfer event handler 메소드를 목록에 등록함
-		for(Method method: this.getClass().getMethods()) {
+		
+		// 현재 클래스의 메소드 목록을 가져옴
+		Method[] methods = this.getClass().getMethods();
+		
+		// 각 메소드 별로 TransferEventHandler Annotation이 있는지 확인하여 등록함
+		// 
+		for(Method method: methods) {
 			
+			// 메소드의 TransferEventHandler Annotation 정보를 가져옴
+			// 없을 경우, 다음 메소드 검사
 			TransferEventHandler handlerAnnotation = method.getAnnotation(TransferEventHandler.class);
 			if(handlerAnnotation == null) {
 				continue;
 			}
 			
+			// 전이 시작 상태 목록 변수
 			String[] sources = handlerAnnotation.source();
+			// 전이 종료 상태 목록 변수
 			String[] targets = handlerAnnotation.target();
 			
+			// 전이 시작 및 종료 상태에 따른 TransferEventHandler 등록
+			// "시작" -> "종료" , method1 형태로 저장 
 			for(String source: sources) {
 			
-				//
+				// 시작 상태가 등록되어 있지 않으면, 등록 수행
 				if(this.transferHandlers.containsKey(source) == false) {
 					this.transferHandlers.put(source, new HashMap<String, ArrayList<Method>>());
 				}
 				
 				HashMap<String, ArrayList<Method>> sourceMap = this.transferHandlers.get(source);
 				
-				//
+				// 종료 상태와 TransferEventHandler 메소드를 등록함
 				for(String target: targets) {
 					
 					if(sourceMap.containsKey(target) == false) {
@@ -64,7 +76,7 @@ public abstract class AbstractParser<T> {
 					
 					ArrayList<Method> handlers = sourceMap.get(target);
 					
-					//
+					// TransferEventHandler의 파라미터 개수는 1개여야 함
 					if(method.getParameterCount() != 1) {
 						throw new Exception("parameter count is not 1");
 					}
@@ -74,22 +86,26 @@ public abstract class AbstractParser<T> {
 						throw new Exception("parameter is not Event type:" + params[0].getType());
 					}
 					
-					//
 					handlers.add(method);
-				}
-			}
-		}
+					
+				} // End of for targets
+				
+			} // End of for sources
+			
+		} // End of for methods
 	}
 	
 	/**
 	 * 시작 상태 반환
-	 * @return
+	 * 
+	 * @return 시작 상태
 	 */
 	protected abstract String getStartStatus();
 	
 	/**
 	 * 종료 상태 목록 반환
-	 * @return
+	 * 
+	 * @return 종료 상태 목록
 	 */
 	protected abstract String[] getEndStatus();
 	
@@ -97,7 +113,7 @@ public abstract class AbstractParser<T> {
 	 * 상태 변환 정보 반환
 	 * -> ex) "A" 상태에서 문자 "B"가 들어오면 "C" 상태로 변한다는 정보
 	 * 
-	 * @return
+	 * @return 상태 변환 정보
 	 */
 	protected abstract HashMap<String, ArrayList<Transfer>> getTransferMap() throws Exception;
 	
@@ -120,10 +136,11 @@ public abstract class AbstractParser<T> {
 	}
 	
 	/**
+	 * 주어진 시작 상태와 종료 상태에 수행되어야 하는 TransferEventHandler 메소드 목록을 반환
 	 * 
-	 * @param source
-	 * @param target
-	 * @return
+	 * @param source 시작 상태
+	 * @param target 종료 상태
+	 * @return TransferEventHandler 메소드 목록
 	 */
 	private ArrayList<Method> getHandlers(String source, String target) throws Exception {
 		
@@ -143,7 +160,10 @@ public abstract class AbstractParser<T> {
 	}
 	
 	/**
+	 * 문자열(script)를 입력받아 파싱 수행 후 루트 노드 반환 
 	 * 
+	 * @param script 파싱할 문자열
+	 * @return 파싱 트리의 루트 노드
 	 */
 	public TreeNode<T> parse(String script) throws Exception {
 		
@@ -153,8 +173,10 @@ public abstract class AbstractParser<T> {
 	}
 	
 	/**
-	 * 파싱 수행
+	 * 문장을 한 글자씩 파싱한 후 파싱 트리의 루트 노드를 반환
+	 * 
 	 * @param in 파싱할 문장의 Reader
+	 * @return 파싱 트리의 루트 노드
 	 */
 	public TreeNode<T> parse(PushbackReader in) throws Exception {
 		
@@ -170,16 +192,19 @@ public abstract class AbstractParser<T> {
 		
 		while(read != -1) {
 			
+			// 입력 문자 변수
 			char ch = (char)read;
 			
-			//
+			// 시작 상태가 전이함수 목록에 없는 경우 예외 발생 
 			if(this.getTransferMap().containsKey(this.status) == false) {
 				throw new Exception("invalid status: " + this.status);
 			}
 			
+			// 유효한 전이함수(현재 상태에서 입력 문자가 있는 경우)가 있는지 여부 변수
 			boolean isMatched = false;
 			
-			//
+			// 전이 함수 목록에서 유효한 전이 함수가 있는지 확인함
+			// 유효한 전이 함수에 따라 상태 변화 후 상태 변화에 따른 TransferEventHandler 메소드를 수행함
 			for(Transfer transferFunction: this.getTransferMap().get(this.status)) {
 				if(transferFunction.isValid(ch) == true) {
 					
@@ -237,14 +262,16 @@ public abstract class AbstractParser<T> {
 	} // End of parse
 	
 	/**
+	 * 현재 노드에 데이터를 설정함
 	 * 
-	 * @param data
+	 * @param data 설정할 데이터
 	 */
 	protected void setNodeData(T data) {
 		this.node.setData(data);
 	}
 	
 	/**
+	 * 현재 노드에 자식 노드를 추가함
 	 * 
 	 * @param data
 	 */
@@ -254,6 +281,7 @@ public abstract class AbstractParser<T> {
 	
 	/**
 	 * 상태 전환시 발생 Event 클래스
+	 * 
 	 * @author jmsohn
 	 */
 	protected static class Event {
